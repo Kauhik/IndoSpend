442cb3cff246a6426c7e9d4212ccf49d6c02b283
Kaushik Manian <kaushikmanian456@gmail.com>
2025-03-07 21:45:28 +0800

    Initial Commit

---
+++ IndoSpendTests/IndoSpendTests.swift
@@ -0,0 +1,17 @@
+//
+//  IndoSpendTests.swift
+//  IndoSpendTests
+//
+//  Created by Kaushik Manian on 7/3/25.
+//
+
+import Testing
+@testable import IndoSpend
+
+struct IndoSpendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
